83e365b909fbdedbb145593ef492e5a505b272db
davidamoen <davidamoen@gmail.com>
2023-10-10 08:27:46 -0500

    scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { FamilyService } from './services/family.service';
+import { RandomizerService } from './services/randomizer.service';
+import { RulesService } from './services/rules.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'giftpicksui';
+export class AppComponent implements OnInit{
+
+constructor(
+  private responsive: BreakpointObserver,
+  private familyService: FamilyService,
+  private randomizerService: RandomizerService,
+  private rulesService: RulesService
+  ) {
+
+}
+
+  ngOnInit(): void {
+
+    this.responsive.observe([
+      Breakpoints.WebLandscape,
+      Breakpoints.HandsetPortrait 
+    ])
+      .subscribe(result => {
+    
+        const breakpoints = result.breakpoints;
+        if (breakpoints[Breakpoints.WebLandscape]) {
+          this.deviceType = "desktop";
+        }
+        else if (breakpoints[Breakpoints.HandsetPortrait]) {
+          this.deviceType = "mobile";
+        }
+    
+      });
+  }
+  title = 'Family Gift Picks';
+  deviceType = 'desktop';
+  family = '';
+
+  families = [
+    "Moen",
+    "Kremer"
+  ]
+
+  getPicks: Diction
+
 }
